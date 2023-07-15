c40d87db0c12f4bdd53c4a1ae9f4b18c55e49662
Matthew Palmer <78506314+MatthewPalmer15@users.noreply.github.com>
2023-07-15 23:42:35 +0100

    Cleaned up some code, added template classes (to be continued).

--- Modular.Core/Objects/Exporter/ModularDataExporter.cs
+++ Modular.Core/Objects/Exporter/ModularDataExporter.cs
@@ -1,20 +1,118 @@
-﻿using DocumentFormat.OpenXml.Packaging;
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
-using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Wordprocessing;
 using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Modular.Core
 {
+
     public static partial class DataExporter
     {
 
+        #region "  Variables  "
 
-        private static void ExportToExcel(DataExporterItem Script, SqlDataReader DataReader)
+        private readonly static string _ScriptPath = AppConfig.GetValue("FolderPath:DataExporterScript");
+
+        private readonly static string _ExportPath = AppConfig.GetValue("FolderPath:DataExporterExport");
+
+        #endregion
+
+        #region "  Properties  "
+
+        public static DirectoryInfo ScriptFolder
+        {
+            get
+            {
+                return new DirectoryInfo(_ScriptPath);
+            }
+        }
+
+        public static DirectoryInfo ExportFolder
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(_ExportPath))
+                {
+                    return new DirectoryInfo(_ExportPath);
+                }
+                else
+                {
+                    return new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads");
+                }
+            }
+        }
+
+        #endregion
+
+        #region "  Public Methods  "
+
+        public static void ExcecuteScript(DataExporterItem Item)
+        {
+            if (Database.CheckDatabaseConnection())
+            {
+                switch (Database.ConnectionMode)
+                {
+                    case Database.DatabaseConnectivityMode.Remote:
+                        using (SqlConnection Connection = new SqlConnection())
+                        {
+                            Connection.Open();
+
+                            using (SqlCommand Command = new SqlCommand())
+                            {
+                                Command.Connection = Connection;
+                                Command.CommandType = CommandType.Text;
+                                Command.CommandText = File.ReadAllText(Item.Name);
+
+                                SqlDataReader DataReader = Command.ExecuteReader();
+                                ExportToFile(Item, DataReader);
+                            }
+
+                            Connection.Close();
+                        }
+
+                        break;
+
+                    case Database.DatabaseConnectivityMode.Local:
+                        break;
+                }
+            }
+            else
+            {
+                throw new ModularException(ExceptionType.DatabaseConnectionError, "Database connection is not available.");
+            }
+        }
+
+        public static List<DataExporterItem> GetAllScripts()
+        {
+           if (ScriptFolder.Exists)
+            {
+                List<DataExporterItem> AllDataExportScripts = new List<DataExporterItem>();
+
+                foreach (FileInfo ScriptFile in ScriptFolder.GetFiles("*.sql"))
+                {
+                    AllDataExportScripts.Add(new DataExporterItem(ScriptFile));
+                }
+                return AllDataExportScripts;
+            }
+           else
+            {
+                ScriptFolder.Create();
+                return new List<DataExporterItem>();
+            }
+        }
+
+        #endregion
+
+        #region "  Private Methods  "
+
+        private static void ExportToFile(DataExporterItem Script, SqlDataReader DataReader)
         {
             using (SpreadsheetDocument ExcelDocument = SpreadsheetDocument.Create(_ExportPath, SpreadsheetDocumentType.Workbook))
             {
@@ -68,7 +166,6 @@ namespace Modular.Core
                     sheetData.AppendChild(dataRow);
                 }
 
-
                 ExcelDocument.Save();
             }
         }
@@ -94,12 +191,13 @@ namespace Modular.Core
             {
                 modulo = (dividend - 1) % 26;
                 columnName = Convert.ToChar(65 + modulo).ToString() + columnName;
-                dividend = (int)((dividend - modulo) / 26);
+                dividend = (dividend - modulo) / 26;
             }
 
             return columnName;
         }
 
 
+        #endregion
     }
 }
