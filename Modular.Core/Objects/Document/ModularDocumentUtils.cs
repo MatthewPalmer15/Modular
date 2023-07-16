66816ceffa9b08ba36615db00fb518df045f1021
Matthew Palmer <78506314+MatthewPalmer15@users.noreply.github.com>
2023-07-16 23:02:51 +0100

    Began on Document Generation

--- Modular.Core/Objects/Document/ModularDocumentUtils.cs
+++ Modular.Core/Objects/Document/ModularDocumentUtils.cs
@@ -3,6 +3,18 @@
     public static class DocumentUtils
     {
 
+        #region "  CREATE Methods  "
+
+        public static Document CreateDocument(string Template)
+        {
+            // return a new document based on the template
+            return new Document();
+        }
+
+        #endregion
+
+        #region "  GET Methods  "
+
         public static Document GetLatestDocument(string DocumentName)
         {
             return GetLatestDocument(DocumentName, DateTime.Now);
@@ -15,7 +27,7 @@
 
         public static Document GetFirstDocument(string DocumentName)
         {
-              return Document.LoadAll(DocumentName).OrderBy(Document => Document.ValidFrom).First();
+            return Document.LoadAll(DocumentName).OrderBy(Document => Document.ValidFrom).First();
         }
 
         public static Document GetSpecificDocument(string DocumentName, int Version)
@@ -23,7 +35,7 @@
             return Document.LoadAll(DocumentName).Find(Document => Document.Version == Version);
         }
 
-
+        #endregion
 
 
 
