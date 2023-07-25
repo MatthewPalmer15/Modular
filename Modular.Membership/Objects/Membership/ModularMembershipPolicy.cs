1a9a1b182dbf8bfa3e90bedc8dca9bdf258ce80e
Matthew Palmer <78506314+MatthewPalmer15@users.noreply.github.com>
2023-07-25 22:57:07 +0100

    Working on the membership module

--- Modular.Membership/Objects/Membership/ModularMembershipPolicy.cs
+++ Modular.Membership/Objects/Membership/ModularMembershipPolicy.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 
 namespace Modular.Membership
 {
-    public class MembershipPolicy : ModularBase
+    public class Policy : ModularBase
     {
 
         #region "  Constructors  "
 
-        public MembershipPolicy() 
+        public Policy() 
         { 
         }
 
@@ -28,7 +28,9 @@ namespace Modular.Membership
 
         private string _PolicyNumber = string.Empty;
 
-        private 
+        private Guid _MemberID;
+
+        private List<Revision> _Revision;
 
         #endregion
 
@@ -50,6 +52,76 @@ namespace Modular.Membership
             }
         }
 
+        public Guid MemberID
+        {
+            get
+            {
+                return _MemberID;
+            }
+            set
+            {
+                if (_MemberID != value)
+                {
+                    _MemberID = value;
+                    OnPropertyChanged("MemberID");
+                }
+            }
+        }
+
+        public List<Revision> Revisions
+        {
+            get
+            {
+                LoadMembershipRevisions();
+                return _Revision;
+            }
+        }
+
+        public Revision ActiveRevision
+        {
+            get
+            {
+                return Revisions.First(Revision => Revision.Status == Core.Utility.StatusType.Verified);
+            }
+        }
+
         #endregion
+
+        #region "  Static Methods  "
+
+        /// <summary>
+        /// Creates a new instance with default values
+        /// </summary>
+        /// <returns>A new instance</returns>
+        public static new Policy Create()
+        {
+            Policy obj = new Policy();
+            obj.SetDefaultValues();
+            return obj;
+        }
+
+        public static new Policy Load(Guid ID)
+        {
+            Policy obj = new Policy();
+            obj.Fetch(ID);
+            return obj;
+        }
+
+        #endregion
+
+        #region "  Instance Methods  "
+
+        private void LoadMembershipRevisions()
+        {
+            _Revision = new List<Revision>().OrderByDescending(Revision => Revision.RevisionNumber).ToList();
+        }
+
+        public override string ToString()
+        {
+            return PolicyNumber;
+        }
+
+        #endregion
+
     }
 }
--- Modular.Membership/Objects/Membership/ModularMembershipRevision.cs
+++ Modular.Membership/Objects/Membership/ModularMembershipRevision.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Modular.Core;
+using Modular.Core.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,138 @@ using System.Threading.Tasks;
 
 namespace Modular.Membership
 {
-    public class MembershipRevision
+    public class Revision : ModularBase
     {
+
+        #region "  Constructors  "
+
+        public Revision() 
+        { 
+        }
+
+        #endregion
+
+        #region "  Constants  "
+
+        protected static new readonly string MODULAR_DATABASE_TABLE = "tbl_Modular_MembershipRevision";
+
+        #endregion
+
+        #region "  Variables  "
+
+        private int _RevisionNumber;
+
+        private Guid _PolicyID;
+
+        private Guid _MembershipID;
+
+        private Guid _InvoiceID;
+
+        private Guid _CreditID;
+
+        private StatusType _Status;
+
+        #endregion
+
+        #region "  Properties  "
+
+        public int RevisionNumber
+        {
+            get
+            {
+                return _RevisionNumber;
+            }
+            set
+            {
+                if (_RevisionNumber != value)
+                {
+                    _RevisionNumber = value;
+                    OnPropertyChanged("RevisionNumber");
+                }
+            }
+        }
+
+        public Guid PolicyID
+        {
+            get
+            {
+                return _PolicyID;
+            }
+            set
+            {
+                if (_PolicyID != value)
+                {
+                    _PolicyID = value;
+                    OnPropertyChanged("PolicyID");
+                }
+            }
+        }
+
+        public Guid MembershipID
+        {
+            get
+            {
+                return _MembershipID;
+            }
+            set
+            {
+                if (_MembershipID != value)
+                {
+                    _MembershipID = value;
+                    OnPropertyChanged("MembershipID");
+                }
+            }
+        }
+
+        public Guid InvoiceID
+        {
+            get
+            {
+                return _InvoiceID;
+            }
+            set
+            {
+                if (_InvoiceID != value)
+                {
+                    _InvoiceID = value;
+                    OnPropertyChanged("InvoiceID");
+                }
+            }
+        }
+
+        public Guid CreditID
+        {
+            get
+            {
+                return _CreditID;
+            }
+            set
+            {
+                if (_CreditID != value)
+                {
+                    _CreditID = value;
+                    OnPropertyChanged("CreditID");
+                }
+            }
+        }
+
+        public StatusType Status
+        {
+            get
+            {
+                return _Status;
+            }
+            set
+            {
+                if (_Status != value)
+                {
+                    _Status = value;
+                    OnPropertyChanged("Status");
+                }
+            }
+        }
+
+        #endregion
+
     }
 }
