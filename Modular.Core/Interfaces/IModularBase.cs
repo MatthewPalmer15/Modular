d5eb506cf0e012697d19fa3f704d3e8519f345ec
Matthew Palmer <78506314+MatthewPalmer15@users.noreply.github.com>
2023-08-23 20:31:17 +0100

    Added interfaces

---
+++ Modular.Core/Interfaces/IModularBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modular.Core.Interfaces
+{
+    public interface IModularBase
+    {
+        public IModularBase Create();
+        public IModularBase Load(Guid ID);
+        public List<IModularBase> LoadAll();
+        public IModularBase Clone();
+
+        public IModularBase GetOrdinals(SqlDataReader DataReader);
+        public IModularBase GetOrdinals(SqliteDataReader DataReader);
+
+    }
+}
